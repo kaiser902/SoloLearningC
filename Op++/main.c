4f095270ab7571920ce1d7665ffef4f78d8cc93d
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-16 14:22:28 +0100

    Op++

---
+++ Op++/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 3;
+    x = x + 1; // x is now 4
+    x++; // x now is 5
+    x += 1; // x now is 6
+    x = x + 1; // is now 7
+    x -= 1; // is now 6
+    x *= 2; // is now 12
+    x /= 2; // is now 6
+    x %= 2; // is now 0
+    printf("La valeur de x est : %d\n",x);
+    return 0;
+}
