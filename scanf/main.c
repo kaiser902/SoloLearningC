b066bd25bd2d69ef6c90bf541987b254b3f8c629
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-16 12:20:44 +0100

    scanf

---
+++ scanf/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long int nbr1;
+    char text;
+    float salary;
+    printf("put your phone number\n");
+    scanf("%ld",&nbr1);
+    printf("put your name : \n");
+    scanf("%s",&text);
+    printf("put your salary : \n");
+    scanf("%f",&salary);
+    return 0;
+}
