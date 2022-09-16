67c18e4f386185a4b3780a65bc1217a0f3144dfc
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-16 11:54:45 +0100

    SumOfTwoNbr

---
+++ SumOfTwoNbr/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float nbr1,nbr2;
+    printf("put your first number :\n");
+    scanf("%f",&nbr1);
+    printf("put your second number :\n");
+    scanf("%f",&nbr2);
+    printf("Thank you\n");
+    printf("Sum is : %2.f\n",nbr1+nbr2);
+    printf("Have a nice day \n");
+    return 0;
+}
