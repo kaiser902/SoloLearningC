56d6ddddb4919ddbc7b8ca1fc911a32637ecc154
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-16 14:16:49 +0100

    Operators

---
+++ Operators/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float average;
+    int total = 13;
+    int count = 37;
+    average = (float) total / count;
+    printf("average = %f\n",average);
+    /* average = 0.351351 */
+    return 0;
+}
