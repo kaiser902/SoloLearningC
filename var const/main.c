dbf14a02fad36a5945f926412f021e158c2cf669
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-16 11:33:41 +0100

    Const var

---
+++ var const/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main()
+{
+    const float Pi = 3.14;
+    printf("%.2f\n",Pi);
+    return 0;
+}
+*/
+#define Pi 3.14
+int main()
+{
+    printf("%.2f\n",Pi);
+    return 0;
+}
