213e45bd4bfe952828e46abc9cfc03b321fee906
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 10:44:08 +0100

    NbrCarre

---
+++ NbrCarre/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nbr;
+    printf("Veuillez saisie le nombre\n");
+    scanf("%d",&nbr);
+    printf("Le carre de nombre %d est : %d\n",nbr,nbr*nbr);
+    return 0;
+}
