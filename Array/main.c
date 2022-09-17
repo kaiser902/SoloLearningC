299e0af85c2992d6c9b4af0da5b9032b002e00c3
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 18:02:29 +0100

    Array

--- Array/main.c
+++ Array/main.c
@@ -5,6 +5,7 @@ int main()
 {
     int test_scores[25]; /* An array size 25 */
     float prices[5] = {3.2, 6.55, 10.49, 1.25, 0.99};
+     printf("The second element is %f\n", prices[1]); /* 6.55 */
 
    // printf("Hello world!\n");
     return 0;
