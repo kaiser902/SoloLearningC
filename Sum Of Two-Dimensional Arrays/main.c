b2c7f15677a29dded1c579e6a02d7671a45a129b
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 18:37:05 +0100

    Sum Of two arrays

---
+++ Sum Of Two-Dimensional Arrays/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[2][3] =
+    {
+        {3, 2, 6},
+        {4, 5, 20}
+    };
+    int sum = 0;
+    for (int k=0;k<2;k++)
+    {
+     for (int j=0;j<3;j++)
+     {
+         sum += a[k][j];
+     }
+    }
+    printf("Sum: %d\n",sum);
+    return 0;
+}
