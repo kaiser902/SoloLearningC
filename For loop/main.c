eacf70fb36afacc03280377bae6733f9d32637ee
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 17:15:53 +0100

    For Loop

--- For loop/main.c
+++ For loop/main.c
@@ -7,4 +7,18 @@ int main() {
     for (i = 0; i < max; i++) {
         printf("%d\n", i);
     }
+
+    /*
+    #include <stdio.h>
+
+int main() {
+    int i=0;
+    int max = 10;
+
+    for (;i < max; i++) {
+        printf("%d\n", i);
+    }
+}
+
+    */
 }
