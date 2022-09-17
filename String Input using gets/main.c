fa3047866ebcc77b047583b377418db9f74b6ac6
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 23:03:41 +0100

    String Input

--- String Input using gets/main.c
+++ String Input using gets/main.c
@@ -6,6 +6,7 @@ int main()
     char full_name[50];
     printf("Enter your full name :\n");
     gets(full_name);
+    // gets(full_name, 50, stdin);
     printf("\nHi, %s.",full_name);
     return 0;
 }
