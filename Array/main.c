91e9be47705c44310c270223fa968b3ff1543891
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-17 17:59:30 +0100

    Array

---
+++ Array/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int test_scores[25]; /* An array size 25 */
+    float prices[5] = {3.2, 6.55, 10.49, 1.25, 0.99};
+
+   // printf("Hello world!\n");
+    return 0;
+}
