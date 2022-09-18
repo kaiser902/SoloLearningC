005d9ee5273de5da1b252dd0ba566ab869b63f0d
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-18 22:53:51 +0100

    Function Pointers

---
+++ Function Pointers/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void say_hello(int num_times); /* function */
+int main()
+{
+    // return_type (*func_name)(parameters)
+    void (*funptr)(int); /* function pointer */
+    funptr = say_hello; /* pointer assignment */
+    funptr(3); /* function call */
+    return 0;
+}
+void say_hello(int num_times){
+int k;
+for (k = 0;k < num_times;k++)
+    printf("Hello\n");
+}
