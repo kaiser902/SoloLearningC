5e747289e154727e936e73c39ccd2d004ff7e43a
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-18 22:22:08 +0100

    The sprintf Functions

---
+++ The sprintf Functions/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char info[100];
+    char dept[] = "HR";
+    int emp = 1337;
+    sprintf(info, "The %s dept has %d employees.", dept, emp);
+    printf("%s\n",info);
+    return 0;
+}
