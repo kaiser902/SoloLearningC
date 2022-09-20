518aeb3a60415015f9df85be5f059bc7ebd442d4
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-20 13:36:36 +0100

    Unions

--- Unions/main.c
+++ Unions/main.c
@@ -11,4 +11,12 @@ int main()
     test.int_num = 1337;
     printf("%d\n",test.int_num);
     return 0;
+    /*
+        union val u1;
+    union val u2;
+    u1.int_num = 1337;
+    u2 = u1;
+    printf("%d", u2.int_num);
+
+    */
 }
