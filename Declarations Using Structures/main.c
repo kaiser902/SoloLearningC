5a061e738a13782dbf6a670183079ee42a3369dc
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-20 12:28:11 +0100

    Declarations Using Structures

--- Declarations Using Structures/main.c
+++ Declarations Using Structures/main.c
@@ -14,5 +14,20 @@ int main() {
     printf("Student: %s, %d\n", s1.name, s1.age);
     printf("Student: %s, %d\n", s2.name, s2.age);
 
+    /*
+        struct student s1; // declaring
+
+    // type cast needed
+    s1= (struct student){19, 9, "John Birghimer"};
+
+    printf("Student: %s, %d\n", s1.name, s1.age);
+
+     */
+     /*
+        struct student s1 = { .grade = 9, .age = 19, .name = "John Birghimer"};
+
+    printf("Name: %s, Age: %d, Grade: %d\n", s1.name, s1.age, s1.grade);
+     */
+
     return 0;
 }
