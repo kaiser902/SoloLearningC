4926394e114d5e25a2b810534fd0f2fefceab153
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-21 14:17:41 +0100

    Memory Management

---
+++ Memory Management/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+       int x;
+    printf("%d", sizeof(x)); /* output: 4 */
+
+    int arr[10];
+    printf("%d", sizeof(arr)); /* output: 40 */
+    return 0;
+}
