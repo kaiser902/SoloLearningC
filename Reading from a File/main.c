3c15c4b015bb3fb123799489d24b6772601f059d
Ait Hammou Anas <lovewithacoco10@gmail.com>
2022-09-21 15:32:23 +0100

    Reading from a File

---
+++ Reading from a File/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  FILE *fptr;
+  int c, stock;
+  char buffer[200], item[10];
+  float price;
+
+  /* myfile.txt: Inventory\n100 Widget 0.29\nEnd of List */
+
+  fptr = fopen("myfile.txt", "r");
+
+  fgets(buffer, 20, fptr);	/* read a line */
+  printf("%s\n", buffer);
+
+  fscanf(fptr, "%d%s%f", &stock, item, &price); /* read data */
+  printf("%d  %s  %4.2f\n", stock, item, price);
+
+  while ((c = getc(fptr)) != EOF) /* read the rest of the file */
+    printf("%c", c);
+
+  fclose(fptr);
+  return 0;
+}
